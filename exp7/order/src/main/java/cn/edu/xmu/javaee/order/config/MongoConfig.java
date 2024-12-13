21686adcdc7c5310de48b407b9fba963d89792e2
llmons <liangjh07@foxmail.com>
2024-12-13 14:32:45 +0800

    exp7:mongo

---
+++ exp7/order/src/main/java/cn/edu/xmu/javaee/order/config/MongoConfig.java
@@ -0,0 +1,36 @@
+//School of Informatics Xiamen University, GPL-3.0 license
+package cn.edu.xmu.javaee.order.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
+    @Bean
+    MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return this.database;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+}
